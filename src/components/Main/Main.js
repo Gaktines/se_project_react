0f1a8aec4c142dc8306b89825f3845f839f23484
Gaktines <119362033+Gaktines@users.noreply.github.com>
2023-06-29 15:45:36 -0600

    js updates

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,15 +2,17 @@ import './Main.css';
 
 const Main = () => {
     return (
+        <>
         <section id="weather">
-            <div>
-                65
+            <div className='weather_tempature'>
+                65 F
             </div>
-<div><img /></div>
+<div><img src="/public/sunnyDay.svg" /></div>
         </section>
         <section id="card-section">
 
         </section>
+        </>  
     )
 }
 
