f8903206a671c981e610633a03298010e686f982
Gaktines <119362033+Gaktines@users.noreply.github.com>
2023-06-30 11:52:43 -0600

    js Header Updates

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,24 @@
-import './Header.css';
+import "./Header.css";
 
 const Header = () => {
-return(
+  return (
     <header className="header">
-        <div className="header__logo">
-            <div>
-                <img src='/images/wtwrLogo.svg' alt='logo'/>
-            </div>
-            <div>Location</div>
+      <div className="header__logo">
+        <div>
+          <img src={require("../../images/wtwrLogo.svg").default} alt="logo" />
         </div>
-        <div className="header__avatar-logo">
-            <div><button type="text">+ New Clothes</button></div>
-            <div>Name</div>
-            <div>
-                <img src='/images/avatar.svg' alt='avatar'/>
-            </div>
+        <div>Location</div>
+      </div>
+      <div className="header__avatar-logo">
+        <div>
+          <button type="text">+ New Clothes</button>
         </div>
+        <div>Name</div>
+        <div>
+          <img src="/images/avatar.svg" alt="avatar" />
+        </div>
+      </div>
     </header>
-
-)
-}
+  );
+};
 export default Header;
