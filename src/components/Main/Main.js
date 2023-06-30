fb65aab830ac917d5677efb7636510a6f3c76deb
Gaktines <119362033+Gaktines@users.noreply.github.com>
2023-06-30 11:18:42 -0600

    js main updates

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -6,7 +6,7 @@ const Main = () => {
       <section id="weather">
         <div className="weather_tempature">65 F</div>
         <div>
-          <img src="./conditions/sunnyDay.svg" alt="conditions" />
+          <img className="weather_conditions" src={require("../images/conditions/sunnyDay.svg").default} alt="conditions" />
         </div>
       </section>
       <section id="card-section"></section>
