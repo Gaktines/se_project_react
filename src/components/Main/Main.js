d8cd08fc1f273a1b77f5c5f0893810e3984be35b
Gaktines <119362033+Gaktines@users.noreply.github.com>
2023-06-30 10:03:53 -0600

    js updates to Main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,19 +1,17 @@
-import './Main.css';
+import "./Main.css";
 
 const Main = () => {
-    return (
-        <>
-        <section id="weather">
-            <div className='weather_tempature'>
-                65 F
-            </div>
-<div><img src="/public/sunnyDay.svg" /></div>
-        </section>
-        <section id="card-section">
-
-        </section>
-        </>  
-    )
-}
+  return (
+    <>
+      <section id="weather">
+        <div className="weather_tempature">65 F</div>
+        <div>
+          <img src="./conditions/sunnyDay.svg" alt="conditions" />
+        </div>
+      </section>
+      <section id="card-section"></section>
+    </>
+  );
+};
 
 export default Main;
